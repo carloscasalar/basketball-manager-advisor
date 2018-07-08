acb95d1ef2f3b3311ff2a3a2f7276ddc33a33663
Carlos Castillo <castillo.st@gmail.com>
2018-07-08 10:11:06 +0200

    Add dependency injector to the project

---
+++ src/ManagerAdvisor/Injector/Injector.php
@@ -0,0 +1,78 @@
+<?php
+    declare(strict_types=1);
+    namespace ManagerAdvisor\Injector;
+
+    use ManagerAdvisor\Domain\RoleRepositoryInterface;
+    use ManagerAdvisor\Domain\TeamMemberRepositoryInterface;
+    use ManagerAdvisor\Persistence\RoleRepository;
+    use ManagerAdvisor\Persistence\StoreManager;
+    use ManagerAdvisor\Persistence\TeamMemberRepository;
+    use ManagerAdvisor\usecase\AddTeamMember;
+
+    class Injector {
+        const PRODUCTION = 'PROD';
+        const TEST = 'TEST';
+
+        /**
+         * @var StoreManager
+         */
+        private $storeManager;
+
+        /**
+         * @var RoleRepositoryInterface
+         */
+        private $roleRepository;
+
+        /**
+         * @var TeamMemberRepositoryInterface
+         */
+        private $teamMemberRepository;
+
+        /**
+         * @var AddTeamMember
+         */
+        private $addTeamMember;
+
+        public function __construct(string $environment = self::PRODUCTION){
+            $storeConfig = [
+              self::PRODUCTION => 'src/ManagerAdvisor/Resources/Store',
+              self::TEST => 'src/ManagerAdvisor/Resources/Test'
+            ];
+
+            $this->storeManager = new StoreManager($storeConfig[$environment]);
+            $this->storeManager->init();
+
+            $this->roleRepository = new RoleRepository($this->storeManager);
+            $this->teamMemberRepository = new TeamMemberRepository($this->storeManager, $this->roleRepository);
+
+            $this->addTeamMember = new AddTeamMember($this->teamMemberRepository);
+        }
+
+        /**
+         * @return StoreManager
+         */
+        public function getStoreManager(): StoreManager {
+            return $this->storeManager;
+        }
+
+        /**
+         * @return RoleRepositoryInterface
+         */
+        public function getRoleRepository(): RoleRepositoryInterface {
+            return $this->roleRepository;
+        }
+
+        /**
+         * @return TeamMemberRepositoryInterface
+         */
+        public function getTeamMemberRepository(): TeamMemberRepositoryInterface {
+            return $this->teamMemberRepository;
+        }
+
+        /**
+         * @return AddTeamMember
+         */
+        public function getAddTeamMember(): AddTeamMember {
+            return $this->addTeamMember;
+        }
+    }
